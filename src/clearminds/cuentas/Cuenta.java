043a923a1bcd892edfec671c4e9451ccc992148f
lopsig <jonathan.ls.trabajo@gmail.com>
2026-08-11 20:41:21 -0500

    feat: update Cuenta class

--- src/clearminds/cuentas/Cuenta.java
+++ src/clearminds/cuentas/Cuenta.java
@@ -25,5 +25,41 @@ public class Cuenta {
         this.saldo = saldo;
     }
 
+    //CONSTRUCTOTES
+    public Cuenta(String id) {
+        this.id = id;
+        this.tipo = "A";
+    }
+
+    public Cuenta(String id, String tipo, double saldo) {
+        this.id = id;
+        this.tipo = tipo;
+        this.saldo = saldo;
+    }
+
+    //METODOS
+    public void imprimir () {
+        System.out.println("************************");
+        System.out.println("        CUENTA          ");
+        System.out.println("************************");
+        System.out.println("Número de Cuenta: " + getId());
+        System.out.println("Tipo: " + getTipo());
+        System.out.println("Saldo: USD " + getSaldo());
+        System.out.println("************************");
+
+
+    }
+
+    public void imprimirConMiEstilo () {
+        System.out.println("************************");
+        System.out.println("  MI ESTILO CUENTA          ");
+        System.out.println("************************");
+        System.out.println("ESTILO Número de Cuenta: " + getId());
+        System.out.println(" ESTILO Tipo: " + getTipo());
+        System.out.println("ESTILO Saldo: USD " + getSaldo());
+        System.out.println("************************");
+
+
+    }
 
 }
